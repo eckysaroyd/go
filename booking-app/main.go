469e6859c55d57bb0a63dd14c30f02f45c0f211a
eckysaroyd <enyato98@gmail.com>
2022-05-24 22:15:30 +0530

    new added for day 2

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,18 +4,26 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var availableTickets = 50
+	const conferenceTickets int = 50
+	var availableTickets uint = 50
 	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, availableTickets)
 	fmt.Println("Get your ticket here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var tickets int
 
-	userName = "Abbu"
-	tickets = 50
-
-	fmt.Printf(" %v has  %v tickets", userName, tickets)
+	fmt.Println("Enter Your First Name \n")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter Your Last Name \n")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter Your Email Name \n")
+	fmt.Scan(&email)
+	fmt.Println("Enter Your Tickets_no \n")
+	fmt.Scan(&tickets)
+	fmt.Printf("Thank you %v   %v for booking %v tickets. You will receive email confirmation at %v \n", firstName, lastName, email, tickets)
+	fmt.Printf("conferenceName type is %T and conferenceTickets type is %T", conferenceName, conferenceTickets)
 
 }
