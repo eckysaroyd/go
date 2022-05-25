f4eee6bef6ac87810c92a3a26448d908d467ea48
eckysaroyd <enyato98@gmail.com>
2022-05-25 20:52:56 +0530

    array using golang

--- booking-app/main.go
+++ booking-app/main.go
@@ -10,12 +10,12 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, availableTickets)
 	fmt.Println("Get your ticket here to attend")
 
+	var booking [50]string
 	var firstName string
 	var lastName string
 	var email string
 	var tickets uint
 
-	availableTickets = availableTickets - tickets
 	fmt.Printf("Enter Your First Name \n")
 	fmt.Scan(&firstName)
 	fmt.Printf("Enter Your Last Name \n")
@@ -24,8 +24,17 @@ func main() {
 	fmt.Scan(&email)
 	fmt.Printf("Enter Your Tickets_no \n")
 	fmt.Scan(&tickets)
+
+	availableTickets = availableTickets - tickets
+	booking[0] = firstName + " " + lastName
+
 	fmt.Printf("Thank you %v   %v for booking %v tickets. You will receive email confirmation at %v \n", firstName, lastName, tickets, email)
 	fmt.Printf("conferenceName type is %T and conferenceTickets type is %T  \n", conferenceName, conferenceTickets)
 	fmt.Printf("\n %v Tickets remains for %v \n", tickets, conferenceName)
+
+	fmt.Printf("\nThe whole array is %v", booking)
+	fmt.Printf("\nThe first array is %v", booking[0])
+	fmt.Printf("\nThe type is %T", booking)
+	fmt.Printf("\nThe size of array is %v", len(booking))
 
 }
