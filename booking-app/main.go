1ba87c7e50e25df8f2dccc5c841061827fa838ce
eckysaroyd <enyato98@gmail.com>
2022-05-26 22:11:02 +0530

    i have done with the first part of sample go small project

--- booking-app/main.go
+++ booking-app/main.go
@@ -29,32 +29,34 @@ func main() {
 		fmt.Printf("Enter Your Tickets_no \n")
 		fmt.Scan(&tickets)
 
-		if availableTickets < tickets {
+		if availableTickets >= tickets {
+			availableTickets = availableTickets - tickets
+			booking = append(booking, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v   %v for booking %v tickets. You will receive email confirmation at %v \n", firstName, lastName, tickets, email)
+			fmt.Printf("conferenceName type is %T and conferenceTickets type is %T  \n", conferenceName, conferenceTickets)
+			fmt.Printf("\n %v Tickets remains for %v \n", tickets, conferenceName)
+
+			fmt.Printf("\nThe whole slice is %v", booking)
+			fmt.Printf("\nThe first slice is %v", booking[0])
+			fmt.Printf("\nThe type is %T", booking)
+			fmt.Printf("\nThe size of slice is %v \n", len(booking))
+			firstNames := []string{}
+			for _, bookings := range booking {
+				names := strings.Fields(bookings)
+				firstNames = append(firstNames, names[0])
+
+			}
+			fmt.Printf("\nThese are all first names %v \n", firstNames)
+
+			if availableTickets == 0 {
+				fmt.Println("Our conference tickect is fully. Comeback next year")
+				break
+			}
+		} else {
 			fmt.Printf("\nWe only have %v Tickets. You cant book %v tickets\n", availableTickets, tickets)
 			continue
 		}
-		availableTickets = availableTickets - tickets
-		booking = append(booking, firstName+" "+lastName)
 
-		fmt.Printf("Thank you %v   %v for booking %v tickets. You will receive email confirmation at %v \n", firstName, lastName, tickets, email)
-		fmt.Printf("conferenceName type is %T and conferenceTickets type is %T  \n", conferenceName, conferenceTickets)
-		fmt.Printf("\n %v Tickets remains for %v \n", tickets, conferenceName)
-
-		fmt.Printf("\nThe whole slice is %v", booking)
-		fmt.Printf("\nThe first slice is %v", booking[0])
-		fmt.Printf("\nThe type is %T", booking)
-		fmt.Printf("\nThe size of slice is %v \n", len(booking))
-		firstNames := []string{}
-		for _, bookings := range booking {
-			names := strings.Fields(bookings)
-			firstNames = append(firstNames, names[0])
-
-		}
-		fmt.Printf("\nThese are all first names %v \n", firstNames)
-
-		if availableTickets == 0 {
-			fmt.Println("Our conference tickect is fully. Comeback next year")
-			break
-		}
 	}
 }
