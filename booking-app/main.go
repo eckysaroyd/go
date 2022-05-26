dcfc1f75d0c803ddf1a44cb808cef7a2d6c7cdba
eckysaroyd <enyato98@gmail.com>
2022-05-26 20:48:34 +0530

    started slice subject

--- booking-app/main.go
+++ booking-app/main.go
@@ -10,7 +10,8 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, availableTickets)
 	fmt.Println("Get your ticket here to attend")
 
-	var booking [50]string
+	// var booking [50]string
+	booking := []string{}
 	var firstName string
 	var lastName string
 	var email string
@@ -26,15 +27,20 @@ func main() {
 	fmt.Scan(&tickets)
 
 	availableTickets = availableTickets - tickets
-	booking[0] = firstName + " " + lastName
+	// booking[0] = firstName + " " + lastName
+	booking = append(booking, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v   %v for booking %v tickets. You will receive email confirmation at %v \n", firstName, lastName, tickets, email)
 	fmt.Printf("conferenceName type is %T and conferenceTickets type is %T  \n", conferenceName, conferenceTickets)
 	fmt.Printf("\n %v Tickets remains for %v \n", tickets, conferenceName)
 
-	fmt.Printf("\nThe whole array is %v", booking)
-	fmt.Printf("\nThe first array is %v", booking[0])
-	fmt.Printf("\nThe type is %T", booking)
-	fmt.Printf("\nThe size of array is %v", len(booking))
+	// fmt.Printf("\nThe whole array is %v", booking)
+	// fmt.Printf("\nThe first array is %v", booking[0])
+	// fmt.Printf("\nThe type is %T", booking)
+	// fmt.Printf("\nThe size of array is %v", len(booking))
 
+	fmt.Printf("\nThe whole slice is %v", booking)
+	fmt.Printf("\nThe first slice is %v", booking[0])
+	fmt.Printf("\nThe type is %T", booking)
+	fmt.Printf("\nThe size of slice is %v", len(booking))
 }
